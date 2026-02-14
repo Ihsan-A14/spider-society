ad3af614493d2dc810b07f232cc7fb7c8aff5fa9
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2026-02-13 21:01:34 -0700

    added meme configuration for one of the memes - going to continue building rest of the program before adding the rest of the memes

---
+++ backend/meme_config.py
@@ -0,0 +1,6 @@
+
+meme_template_db = {'drake_meme': {
+    'filename': 'components/Drake_Hotline_Blind.png',
+    'top_text': {"pos": (430, 15), "box_size": (340, 370), "align": "center", "color": "black","font_size": 60,"align": "center"},
+    'bot_text': {"pos": (430, 415), "box_size": (340, 370), "align": "center", "color": "black","font_size": 60,"align": "center"}
+    }}
