b10c5bdbf8aadf8f0b80b3a20ec0dc80cdd59a6e
Ihsan Muhammad Anjakkulam <ihsanmuhammad@Ihsans-MacBook-Pro.local>
2026-02-14 17:18:46 -0700

    changed the text box sizes

--- backend/meme_config.py
+++ backend/meme_config.py
@@ -1,59 +1,60 @@
 meme_template_db = {
     'drake_meme': {
         'filename': 'components/Drake_Hotline_Blind.png',
-        'top_text': {"pos": (430, 15), "box_size": (340, 370), "align": "center", "color": "black", "font_size": 60},
-        'bot_text': {"pos": (430, 415), "box_size": (340, 370), "align": "center", "color": "black", "font_size": 60}
+        # Reduced box sizes for padding
+        'top_text': {"pos": (440, 25), "box_size": (310, 340), "align": "center", "color": "black", "font_size": 60},
+        'bot_text': {"pos": (440, 425), "box_size": (310, 340), "align": "center", "color": "black", "font_size": 60}
     },
 
     "dicaprio_laugh": {
         "filename": "components/dicaprio_laughing.png",
-        "top_text": {"pos": (15, 15), "box_size": (1220, 170), "color": "black", "font_size": 60, "align": "left"},
-        "bot_text": {"pos": (0, 0), "box_size": (0, 0), "color": None, "font_size": 1}
+        'top_text': {"pos": (30, 30), "box_size": (1180, 140), "color": "black", "font_size": 60, "align": "left"},
+        'bot_text': {"pos": (0, 0), "box_size": (0, 0), "color": None, "font_size": 1}
     },
 
     "panda_suit": {
         "filename": "components/panda_suit.png",
-        "top_text": {"pos": (565, 15), "box_size": (720, 435), "color": "black", "font_size": 50, "align": "center"},
-        "bot_text": {"pos": (565, 490), "box_size": (720, 432), "color": "black", "font_size": 50, "align": "center"}
+        'top_text': {"pos": (620, 35), "box_size": (620, 395), "color": "black", "font_size": 50, "align": "center"},
+        'bot_text': {"pos": (620, 510), "box_size": (620, 392), "color": "black", "font_size": 50, "align": "center"}
     },
 
     "clown_makeup": {
         "filename": "components/clown.png",
-        "text_1": {"pos": (15, 13), "box_size": (585, 217), "color": "black", "font_size": 40, "align": "center"},
-        "text_2": {"pos": (18, 270), "box_size": (552, 233), "color": "black", "font_size": 40, "align": "center"},
-        "text_3": {"pos": (19, 552), "box_size": (518, 217), "color": "black", "font_size": 40, "align": "center"},
-        "text_4": {"pos": (23, 809), "box_size": (538, 215), "color": "black", "font_size": 40, "align": "center"}
+        'text_1': {"pos": (30, 28), "box_size": (555, 187), "color": "black", "font_size": 40, "align": "center"},
+        'text_2': {"pos": (33, 285), "box_size": (522, 203), "color": "black", "font_size": 40, "align": "center"},
+        'text_3': {"pos": (34, 567), "box_size": (488, 187), "color": "black", "font_size": 40, "align": "center"},
+        'text_4': {"pos": (38, 824), "box_size": (508, 185), "color": "black", "font_size": 40, "align": "center"}
     },
 
     "brain_explode": {
         "filename": "components/brain_explode.png",
-        "text_1": {"pos": (20, 20), "box_size": (350, 275), "color": "black", "font_size": 40, "align": "center"},
-        "text_2": {"pos": (20, 320), "box_size": (350, 275), "color": "black", "font_size": 40, "align": "center"},
-        "text_3": {"pos": (22, 632), "box_size": (366, 203), "color": "black", "font_size": 40, "align": "center"},
-        "text_4": {"pos": (27, 907), "box_size": (346, 219), "color": "black", "font_size": 40, "align": "center"}
+        'text_1': {"pos": (35, 35), "box_size": (320, 245), "color": "black", "font_size": 40, "align": "center"},
+        'text_2': {"pos": (35, 335), "box_size": (320, 245), "color": "black", "font_size": 40, "align": "center"},
+        'text_3': {"pos": (37, 647), "box_size": (336, 173), "color": "black", "font_size": 40, "align": "center"},
+        'text_4': {"pos": (42, 922), "box_size": (316, 189), "color": "black", "font_size": 40, "align": "center"}
     },
 
     'woman_cat':{
         'filename': 'components/woman_cat.png',
-        'top_text': {"pos": (11, 14), "box_size": (744, 181), "align": "center", "color": "black", "font_size": 60},
-        'bot_text': {"pos": (808, 17), "box_size": (712, 175), "align": "center", "color": "black", "font_size": 60}
+        'top_text': {"pos": (25, 29), "box_size": (714, 151), "align": "center", "color": "black", "font_size": 60},
+        'bot_text': {"pos": (823, 32), "box_size": (682, 145), "align": "center", "color": "black", "font_size": 60}
     },
 
     'puppet_scared': {
-    'filename': 'components/puppet_scared.png',
-    'top_text': {"pos": (20, 25), "box_size": (540, 321), "align": "center", "color": "black", "font_size": 30},
-    'bot_text': {"pos": (654, 28), "box_size": (550, 318), "align": "center", "color": "black", "font_size": 30}
+        'filename': 'components/puppet_scared.png',
+        'top_text': {"pos": (35, 40), "box_size": (510, 291), "align": "center", "color": "black", "font_size": 30},
+        'bot_text': {"pos": (669, 43), "box_size": (520, 288), "align": "center", "color": "black", "font_size": 30}
      },
     
     'two_button': {
-    'filename': 'components/two_button.png',
-    'top_text': {"pos": (70, 120), "box_size": (287, 63), "align": "center", "color": "black", "font_size": 60},
-    'bot_text': {"pos": (385, 85), "box_size": (270, 100), "align": "center", "color": "black", "font_size": 60}
-},
+        'filename': 'components/two_button.png',
+        'top_text': {"pos": (80, 130), "box_size": (267, 43), "align": "center", "color": "black", "font_size": 60},
+        'bot_text': {"pos": (395, 95), "box_size": (250, 80), "align": "center", "color": "black", "font_size": 60}
+    },
     
     'girl_distress': {
-    'filename': 'components/girl_distress.png',
-    'top_text': {"pos": (36, 648), "box_size": (749, 40), "align": "center", "color": "white", "font_size": 1200},
-    'bot_text': {"pos": (42, 856), "box_size": (759, 50), "align": "center", "color": "white", "font_size": 1200}
-},
+        'filename': 'components/girl_distress.png',
+        'top_text': {"pos": (50, 648), "box_size": (720, 40), "align": "center", "color": "white", "font_size": 40},
+        'bot_text': {"pos": (56, 856), "box_size": (730, 50), "align": "center", "color": "white", "font_size": 50}
+    },
 }
